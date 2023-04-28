8c58be27564cdd970373f78f13c0dbb51ab820f2
canberk-oguz <c.oguz@elsevier.com>
2023-04-28 14:19:18 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Prerequisites
+- Java 17
+
+```
+git clone git@github.com:canberk-oguz/els-lss-interview-demo.git
+cd els-lss-interview-demo
+./mvnw spring-boot:run
+```
---
+++ src/test/java/com/elsevier/lssinterviewdemo/InterviewDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elsevier.lssinterviewdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
